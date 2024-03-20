df1d49659c40a2c9536a38e14bf7b60fa23460dd
Yigit Akture <220706301@st.maltepe.edu.tr>
2024-03-20 18:24:07 +0300

    Login - Register Frontend

---
+++ ProjectManagament-WebApp/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagament_WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectManagament-WebApp/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagament_WebApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
