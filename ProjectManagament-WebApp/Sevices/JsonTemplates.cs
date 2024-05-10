f8bd36c2245652f7073093c87d0dd1c3f38abd4d
Erman Yalçın <ermanylcn1103@gmail.com>
2024-05-11 00:25:12 +0300

    JsonTemplates created for saving Modüle needs
    Co-authored-by: Sena Yılmaz <ylmz_sena@hotmail.com>

---
+++ ProjectManagament-WebApp/Sevices/JsonTemplates.cs
@@ -0,0 +1,33 @@
+﻿namespace ProjectManagament_WebApp.Sevices
+{
+    public class JsonTemplates
+    {
+        public Dictionary<Guid, string> Templates { get; set; }
+
+        public JsonTemplates()
+        {
+            Templates = new Dictionary<Guid, string>
+        {
+            { new Guid("your-module-id-1"), @"
+                {modüle göre jsonlar!!}"
+            },
+            { new Guid("your-module-id-2"), @"
+                {modüle göre jsonlar!!}"
+            },
+            { new Guid("your-module-id-3"), @"
+                {modüle göre jsonlar!!}"
+            }
+        };
+        }
+
+        public string GetTemplate(Guid moduleId)
+        {
+            if (Templates.TryGetValue(moduleId, out var template))
+            {
+                return template;
+            }
+            return "";
+        }
+    }
+
+}
