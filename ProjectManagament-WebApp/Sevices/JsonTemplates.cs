ad16072c66d8597abf1ba344de92b1fe0fd3d4fe
Erman Yalçın <ermanylcn1103@gmail.com>
2024-05-11 02:41:21 +0300

    Revert "Editing Json Templates - Updating the Json Templates for modüles"
    This reverts commit aa15f007799ebaf841db191ae52fa9c140b09de8.

--- ProjectManagament-WebApp/Sevices/JsonTemplates.cs
+++ ProjectManagament-WebApp/Sevices/JsonTemplates.cs
@@ -4,120 +4,18 @@
     {
         public Dictionary<Guid, string> Templates { get; set; }
 
-        //GUID ID'ler güncellenmeli!!!
         public JsonTemplates()
         {
             Templates = new Dictionary<Guid, string>
         {
-            { new Guid("29931809-ec70-433b-9ecf-def9c7189e39"), @"You are a company searcher assistant who gives all available data you can find, can search on internet and designed to fill and return only this JSON:
-                {
-    ""companyName"": ""Example Company Inc."",
-    ""industry"": ""Technology"",
-    ""servicesProducts"": [
-      ""Software Development"",
-      ""Cloud Services""
-    ],
-    ""headquartersLocation"": {
-      ""address"": ""1234 Business Rd."",
-      ""city"": ""Tech City"",
-      ""state"": ""Innovation"",
-      ""country"": ""Techland""
-    },
-    ""branchLocations"": [
-      {
-        ""address"": ""4321 Market St."",
-        ""city"": ""Commerce City"",
-        ""state"": ""Enterprise"",
-        ""country"": ""Businessland""
-      }
-    ],
-    ""contactInformation"": {
-      ""generalInquiryPhone"": ""+1-555-0100"",
-      ""customerService"": ""+1-555-0200"",
-      ""departments"": {
-        ""sales"": ""+1-555-0300"",
-        ""support"": ""+1-555-0400""
-      }
-    },
-    ""emailAddresses"": {
-      ""generalInquiry"": ""info@example.com"",
-      ""support"": ""support@example.com"",
-      ""sales"": ""sales@example.com""
-    },
-    ""websiteURL"": ""http://www.example.com"",
-    ""socialMediaProfiles"": {
-      ""LinkedIn"": ""http://www.linkedin.com/company/example-company-inc"",
-      ""Twitter"": ""http://twitter.com/ExampleCompany"",
-      ""Facebook"": ""http://www.facebook.com/ExampleCompany"",
-      ""Instagram"": ""http://www.instagram.com/ExampleCompany""
-    },
-    ""keyExecutives"": {
-      ""CEO"": ""John Doe"",
-      ""CFO"": ""Jane Doe"",
-      ""CTO"": ""Jim Beam""
-    },
-    ""companyHistory"": ""Example Company was founded in 2000, with a mission to innovate..."",
-    ""missionStatement"": ""To lead in the creation of innovative solutions..."",
-    ""certificationsAwards"": [
-      ""ISO 9001 Certified"",
-      ""Best Tech Company 2023""
-    ],
-    ""clientelePartnerships"": [
-      ""BigTech Inc."",
-      ""Innovate LLC""
-    ],
-    ""financialInformation"": {
-      ""stockSymbol"": ""EXMPL"",
-      ""recentFinancialPerformance"": ""Profitable Q1 with a 10% increase in revenue"",
-      ""marketData"": ""Listed in Techland Stock Exchange""
-    },
-    ""pressReleasesMedia"": ""http://www.example.com/news"",
-    ""careerOpportunities"": ""http://www.example.com/careers"",
-    ""FAQsSupportSection"": ""http://www.example.com/support""
-  }. Only Return the filled Json Format do not write anything else!"
+            { new Guid("your-module-id-1"), @"
+                {modüle göre jsonlar!!}"
             },
-
-
-            { new Guid("394ad26d-99bf-48ff-bcd7-332d5c66f7ce"), @"
-                You are a job search assistant that specializes in finding job listings for specific roles and locations based on user input. When a user provides a job title, such as ""welder"", you will search for welding job opportunities globally. If the user specifies a job title along with a location, e.g., ""welder in Bursa"", you will narrow down the search to that particular location. Your task is to find these listings, extract key details about the job opportunities, you can search on internet and format this information into a structured JSON response as follows:
-
-{
-    ""job_1"": {
-        ""job_title"": ""example_job_title_1"",
-        ""job_location"": ""example_job_location_1"",
-        ""company_name"": ""example_company_name_1"",
-        ""contact_info"": ""example_contact_info_1""
-    },
-    ""job_2"": {
-        ""job_title"": ""example_job_title_2"",
-        ""job_location"": ""example_job_location_2"",
-        ""company_name"": ""example_company_name_2"",
-        ""contact_info"": ""example_contact_info_2""
-    },
-    ... (continues like this for all found jobs)
-}
-
-Ensure that you only return the information in JSON format without any additional text or explanation. The system should perform searches dynamically based on the input and provide the most current job listings available.
-"
+            { new Guid("your-module-id-2"), @"
+                {modüle göre jsonlar!!}"
             },
-
-
-            { new Guid("7c1f7fc4-ab3a-486f-b7fd-4ea9b99b2571"), @"
-                {You are a company searcher assistant who gives all company name in a given business area. Give at least 10 of them. Yu can make search on internet. If only company name entered make an global search if company name and location entered give the result in an given location."" 
-""designed to fill and return only this JSON format""
-JSON: {
-    ""company_1"": {
-          ""company_name_1"": ""example_company_name_1"",
-          ""company_location_1"": ""example_company_location_1"",
-          ""company_gsm_1"": ""example_company_gsm_1"",
-     },
-     ""company_2"": {
-          ""company_name_2"": ""example_company_name_1"",
-          ""company_location_2"": ""example_company_location_1"",
-          ""company_gsm_2"": ""example_company_gsm_1"",
-     },
-     ... (continues like that)
-} Gİve the result only witn json format, do not add exstra information.}"
+            { new Guid("your-module-id-3"), @"
+                {modüle göre jsonlar!!}"
             }
         };
         }
