250284dc92db05fa116cca057ec676c7253766ec
Erman Yalçın <ermanylcn1103@gmail.com>
2024-05-28 23:55:32 +0300

    Tests Added v2
    Co-authored-by: Sena Yılmaz <ylmz_sena@hotmail.com>

---
+++ ProjectManagement-WebApp.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using Moq;
+using ProjectManagament_WebApp.Controllers;
+using ProjectManagament_WebApp.Data;
+using ProjectManagament_WebApp.Sevices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class UserControllerTests
+{
+    [Fact]
+    public void Index_ReturnsViewResult()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<PMContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var mockContext = new PMContext(options);
+
+        var mockEmailService = new Mock<IEmailService>();
+        var controller = new UserController(mockContext, mockEmailService.Object);
+
+        // Act
+        var result = controller.Index();
+
+        // Assert
+        Assert.IsType<ViewResult>(result);
+    }
+
+    [Fact]
+    public void ForgotPassword_ReturnsViewResult()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<PMContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        var mockContext = new PMContext(options);
+
+        var mockEmailService = new Mock<IEmailService>();
+        var controller = new UserController(mockContext, mockEmailService.Object);
+
+        // Act
+        var result = controller.ForgotPassword();
+
+        // Assert
+        Assert.IsType<ViewResult>(result);
+    }
+}
---
+++ ProjectManagement-WebApp.Tests/Helpers/MockDbSetHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class MockDbSetHelper
+{
+    public static Mock<DbSet<T>> BuildMockDbSet<T>(this List<T> sourceList) where T : class
+    {
+        var queryable = sourceList.AsQueryable();
+        var dbSet = new Mock<DbSet<T>>();
+        dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+        dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+        dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+        dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+        dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>(sourceList.Add);
+        return dbSet;
+    }
+}
